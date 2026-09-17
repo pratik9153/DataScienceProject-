404ea6c5af4ba258f5d02685d5733c7f14695706
pratik9153 <pratikchoudhary316@gmail.com>
2026-09-17 22:53:06 +0530

    project flow

---
+++ README.md
@@ -0,0 +1,21 @@
+# End to End Data science project
+
+### workflows -- Ml Pipeline
+
+1. Data Ingestion 
+2. Data Validation
+3. Data Transformation
+4. Model Trainer
+5. Model Evaluation 
+
+
+## Workflows
+
+1. Update config.yaml
+2. Update schema.yaml
+3. Update params.yaml
+4. Update the entity
+5. update the configuration manager in src config
+6. update the components
+7. update the pipeline
+8. update the main.py
--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from src.datascienceproject import logger
+from src.datascienceproject.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 
 logger.info("Welcome to logging")
+
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.initiate_data_ingestion()
+    logger.info(f">>>>>  stage {STAGE_NAME} completed <<<<<<\n\nx=====x")
+except Exception as e:
+    logger.exception(e)
+    raise e
