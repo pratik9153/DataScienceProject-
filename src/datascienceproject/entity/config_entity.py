4954cf822d687ead54105e6a8f7a4c7a89e1ba57
pratik9153 <pratikchoudhary316@gmail.com>
2026-09-18 12:45:15 +0530

    project flow 2

--- src/datascienceproject/entity/config_entity.py
+++ src/datascienceproject/entity/config_entity.py
@@ -8,4 +8,11 @@ class DataIngestionConfig:
     local_data_file: Path
     unzip_dir: Path
 
-    
+
+
+@dataclass
+class DataValidationConfig:
+    root_dir:Path
+    STATUS_FILE:str
+    unzip_data_dir: Path
+    all_schema: dict
